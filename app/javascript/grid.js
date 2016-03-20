1e885ecb78f18f80eb9bc89bfd5b26da512e7bab
Darren Labithiotis <labithiotis@Darrens-Air.home>
2016-03-20 09:51:03 +0000

    Added functionality to update blocky board on blockClicked and added test to support this

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -1,58 +1,146 @@
 export const COLOURS = ['red', 'green', 'blue', 'yellow'];
+export const EMPTY_COLOUR = 'black';
 const MAX_X = 10;
 const MAX_Y = 10;
 
 export class Block {
-    constructor (x, y) {
-        this.x = x;
-        this.y = y;
-        this.colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
-    }
+	constructor(x, y, blank, colour) {
+		this.x = x;
+		this.y = y;
+		if(blank) {
+			this.blank = !!blank;
+		} else {
+			this.colour = colour ? colour : COLOURS[Math.floor(Math.random() * COLOURS.length)];
+		}
+	}
 }
 
 export class BlockGrid {
-    constructor () {
-        this.grid = [];
 
-        for (let x = 0; x < MAX_X; x++) {
-            let col = [];
-            for (let y = 0; y < MAX_Y; y++) {
-                col.push(new Block(x, y));
-            }
+	constructor(maxX = MAX_X, maxY = MAX_Y, pattern) {
+		this.grid = [];
+		this.maxX = maxX;
+		this.maxY = maxY;
 
-            this.grid.push(col);
-        }
+		for(let x = 0; x < this.maxX; x++) {
+			let col = [];
+			for(let y = 0; y < this.maxY; y++) {
+				col.push(new Block(x, y, null, pattern ? pattern[x][y] : null));
+			}
 
-        return this;
-    }
+			this.grid.push(col);
+		}
 
-    render (el = document.querySelector('#gridEl')) {
-        for (let x = 0; x < MAX_X; x++) {
-            let id = 'col_' + x;
-            let colEl = document.createElement('div');
-            colEl.className = 'col';
-            colEl.id = id;
-            el.appendChild(colEl);
+		//console.log(this.grid);
 
-            for (let y = MAX_Y - 1; y >= 0; y--) {
-                let block = this.grid[x][y],
-                    id = `block_${x}x${y}`,
-                    blockEl = document.createElement('div');
+		return this;
+	}
 
-                blockEl.id = id;
-                blockEl.className = 'block';
-                blockEl.style.background = block.colour;
-                blockEl.addEventListener('click', (evt) => this.blockClicked(evt, block));
-                colEl.appendChild(blockEl);
-            }
-        }
+	render(el = document.querySelector('#gridEl')) {
+		
+		el.innerHTML = '';
+		
+		for(let x = 0; x < this.maxX; x++) {
+			let id = 'col_' + x;
+			let colEl = document.createElement('div');
+			colEl.className = 'col';
+			colEl.id = id;
+			el.appendChild(colEl);
 
-        return this;
-    }
+			for(let y = this.maxY - 1; y >= 0; y--) {
 
-    blockClicked (e, block) {
-        console.log(e, block);
-    }
+				let block = this.grid[x][y],
+					id = `block_${x}x${y}`,
+					blockEl = document.createElement('div');
+
+				blockEl.id = id;
+				blockEl.className = 'block';
+
+				if(block.blank) {
+					blockEl.style.background = EMPTY_COLOUR;
+				} else {
+					blockEl.style.background = block.colour;
+					//blockEl.innerText = `${x},${y}  t:${block.tagForRemoval || ''}`;
+					blockEl.addEventListener('click', (evt) => this.blockClicked(evt, block));
+				}
+
+				colEl.appendChild(blockEl);
+
+			}
+		}
+
+		return this;
+	}
+
+	blockClicked(e, block) {
+		this.removeAdjacentBlocks(block);
+		this.updateBlocksXY();
+		this.render();
+		//console.log('block', block)
+		//console.log('after', this.grid)
+	}
+
+	removeAdjacentBlocks(block) {
+
+		try {
+
+			let blocks = [
+				// Above block
+				block.y < this.maxY ? this.grid[block.x][block.y + 1] : null,
+				// Right block
+				block.x < this.maxX - 1 ? this.grid[block.x + 1][block.y] : null,
+				// Below block
+				block.y > 0 ? this.grid[block.x][block.y - 1] : null,
+				// Left Block
+				block.x > 0 ? this.grid[block.x - 1][block.y] : null
+			];
+
+			//console.log(blocks);
+
+			// Remove self
+			block.tagForRemoval = true;
+
+			// Remove all adjacent blocks that match in colour
+			blocks.forEach(adjacentBlock => {
+				if(adjacentBlock && !adjacentBlock.tagForRemoval && adjacentBlock.colour == block.colour) {
+					adjacentBlock.tagForRemoval = true;
+					this.removeAdjacentBlocks(adjacentBlock);
+				}
+			});
+
+			this.removeTaggedBlocks();
+
+		} catch(y) {
+			console.error(y);
+			return false;
+		}
+
+	}
+
+	removeTaggedBlocks() {
+
+		this.grid.forEach((column, x) => {
+			column.forEach((block, y) => {
+				if(block.tagForRemoval) {
+					this.grid[x].splice(y, 1);
+					this.grid[x].push(new Block(x, this.maxY, true));
+				}
+			});
+		});
+
+	}
+
+	updateBlocksXY() {
+
+		this.grid.forEach((column, x) => {
+			column.forEach((block, y) => {
+				block.x = x;
+				block.y = y;
+			});
+		});
+
+	}
+
 }
 
 window.addEventListener('DOMContentLoaded', () => new BlockGrid().render());
--- test/tests.js
+++ test/tests.js
@@ -1,27 +1,92 @@
-import { Block, COLOURS } from '../app/javascript/grid';
-import { assert } from 'chai';
+import {Block, COLOURS, BlockGrid, EMPTY_COLOUR} from '../app/javascript/grid';
+import {assert} from 'chai';
 
-let { describe, it } = window;
+let {describe, it} = window;
+
+describe('Type Checker', () => {
+	it('EMPTY_STRING should be string', () => {
+		assert.ok(typeof EMPTY_COLOUR === 'string')
+	});
+	it('COLOURS should be an Array', () => {
+		assert.ok(COLOURS instanceof Array)
+	});
+	it('Block should be a Class', () => {
+		assert.ok(typeof Block === 'function');
+	});
+	it('BlockGrid should be a Class', () => {
+		assert.ok(typeof Block === 'function');
+	});
+});
 
 describe('Block', () => {
 
-    it('should be created with correctly', () => {
-        let testCoords = [
-            [1, 2],
-            [4, 9],
-            [0, 0]
-        ];
+	it('should be created with correctly', () => {
+		let testCoords = [
+			[1, 2],
+			[4, 9],
+			[0, 0]
+		];
 
-        testCoords.forEach((testCoord) => {
-            let block = new Block(...testCoord);
-            assert.equal(block.x, testCoord[0], 'x is set correctly');
-            assert.equal(block.y, testCoord[1], 'y is set correctly');
-            assert.ok(COLOURS.indexOf(block.colour) > -1, 'colour is valid');
-        });
-    });
+		testCoords.forEach((testCoord) => {
+			let block = new Block(...testCoord);
+			assert.equal(block.x, testCoord[0], 'x is set correctly');
+			assert.equal(block.y, testCoord[1], 'y is set correctly');
+			assert.ok(COLOURS.indexOf(block.colour) > -1, 'colour is valid');
+		});
+	});
+
+	it('blank block should be created correctly', () => {
+
+		let block = new Block(1, 1, true);
+
+		assert.equal(block.blank, true);
+
+	});
 
 });
 
 describe('BlockGrid', () => {
+	
+
+
+	it('should be created correctly', () => {
+
+		const blockGrid = new BlockGrid(5, 5);
+		
+		assert.ok(blockGrid.grid instanceof Array);
+		assert.ok(blockGrid.grid[0][0] instanceof Block);
+		assert.equal(blockGrid.grid.length, 5);
+		assert.equal(blockGrid.grid[0].length, 5);
+		
+	});
+
+	it('should update correctly', () => {
+
+		let pattern = [
+			['red', 'red', 'blue', 'green', 'yellow'],
+			['red', 'red', 'yellow', 'yellow', 'yellow'],
+			['blue', 'blue', 'yellow', 'green', 'green'],
+			['yellow', 'yellow', 'yellow', 'yellow', 'yellow'],
+			['blue', 'blue', 'blue', 'blue', 'blue']
+		];
+
+		const blockGrid = new BlockGrid(5, 5, pattern);
+
+		blockGrid.render = function() {};
+
+		blockGrid.blockClicked(null, blockGrid.grid[0][0]);
+
+		assert.equal(blockGrid.grid[0][0].colour, 'blue');
+		assert.equal(blockGrid.grid[1][0].colour, 'yellow');
+
+		blockGrid.blockClicked(null, blockGrid.grid[1][0]);
+
+		assert.equal(blockGrid.grid[3][0].blank, true);
+
+		blockGrid.blockClicked(null, blockGrid.grid[4][4]);
+		assert.equal(blockGrid.grid[4].length, 5);
+
+	});
+	
 
 });
